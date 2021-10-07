7d20e3e56a8aab259f359956366206f161d51130
Masayuki Sugahara <equaaqua@hotmail.com>
2021-10-07 22:27:19 +0900

    add: ts & eslint

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+const jigenRawData = document.getElementsByClassName('kyuko-date-jigen');
+if (jigenRawData.length)
+    download();
+const frame = document.getElementsByName('topmenu')[0];
+frame.addEventListener('load', () => {
+    frame.contentDocument.getElementById('timeout').parentElement.parentElement.innerText = "タイムアウト抑制中";
+    setTimeout(() => {
+        frame.contentWindow.location.reload();
+    }, 1700000);
+});
+//# sourceMappingURL=main.js.map
